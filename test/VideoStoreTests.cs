a404e4dd89a27dac2b1ba4741a59078fa0c3d77b
Daniel Shaefer <daniel.shaefer@hudl.com>
2017-11-27 16:30:17 -0600

    fixed movie titles

--- test/VideoStoreTests.cs
+++ test/VideoStoreTests.cs
@@ -9,40 +9,40 @@ namespace VideoStore
 
         public VideoStoreTests()
         {
-            customer = new Customer("Fred");
+            customer = new Customer("Casey");
         }
 
   
         [Fact]
         public void testSingleNewReleaseStatement()
         {
-            customer.AddRental(new Rental(new Movie("The Cell", PriceCode.NewRelease), 3));
-            Assert.Equal("Rental record for Fred\n\tThe Cell\t9.0\nAmount owed is 9.0\nYou earned 2 frequent renter points.", customer.Statement());
+            customer.AddRental(new Rental(new Movie("Thor: Ragnarok", PriceCode.NewRelease), 3));
+            Assert.Equal("Rental record for Casey\n\tThor: Ragnarok\t9.0\nAmount owed is 9.0\nYou earned 2 frequent renter points.", customer.Statement());
         }
 
         [Fact]
         public void testDualNewReleaseStatement()
         {
-            customer.AddRental(new Rental(new Movie("The Cell", PriceCode.NewRelease), 3));
-            customer.AddRental(new Rental(new Movie("The Tigger Movie", PriceCode.NewRelease), 3));
-            Assert.Equal("Rental record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nAmount owed is 18.0\nYou earned 4 frequent renter points.", customer.Statement());
+            customer.AddRental(new Rental(new Movie("Thor: Ragnarok", PriceCode.NewRelease), 3));
+            customer.AddRental(new Rental(new Movie("Justice League", PriceCode.NewRelease), 3));
+            Assert.Equal("Rental record for Casey\n\tThor: Ragnarok\t9.0\n\tJustice League\t9.0\nAmount owed is 18.0\nYou earned 4 frequent renter points.", customer.Statement());
         }
 
         [Fact]
         public void testSingleChildrensStatement()
         {
-            customer.AddRental(new Rental(new Movie("The Tigger Movie", PriceCode.Childrens), 3));
-            Assert.Equal("Rental record for Fred\n\tThe Tigger Movie\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points.", customer.Statement());
+            customer.AddRental(new Rental(new Movie("The Last Unicorn", PriceCode.Childrens), 3));
+            Assert.Equal("Rental record for Casey\n\tThe Last Unicorn\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points.", customer.Statement());
         }
 
         [Fact]
         public void testMultipleRegularStatement()
         {
-            customer.AddRental(new Rental(new Movie("Plan 9 from Outer Space", PriceCode.Regular), 1));
-            customer.AddRental(new Rental(new Movie("8 1/2", PriceCode.Regular), 2));
-            customer.AddRental(new Rental(new Movie("Eraserhead", PriceCode.Regular), 3));
+            customer.AddRental(new Rental(new Movie("The Matrix", PriceCode.Regular), 1));
+            customer.AddRental(new Rental(new Movie("6 Days 7 Nights", PriceCode.Regular), 2));
+            customer.AddRental(new Rental(new Movie("The Man Who Knew Too Little", PriceCode.Regular), 3));
 
-            Assert.Equal("Rental record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nAmount owed is 7.5\nYou earned 3 frequent renter points.", customer.Statement());
+            Assert.Equal("Rental record for Casey\n\tThe Matrix\t2.0\n\t6 Days 7 Nights\t2.0\n\tThe Man Who Knew Too Little\t3.5\nAmount owed is 7.5\nYou earned 3 frequent renter points.", customer.Statement());
         }
 
     }
