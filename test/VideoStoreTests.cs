4d25c2f913a5b4267f0086ec659bbc60739d5ef3
Daniel Shaefer <daniel.shaefer@hudl.com>
2017-11-27 15:31:44 -0600

    Fixed several text details and formatting issues for tests

--- test/VideoStoreTests.cs
+++ test/VideoStoreTests.cs
@@ -17,7 +17,7 @@ namespace VideoStore
         public void testSingleNewReleaseStatement()
         {
             customer.AddRental(new Rental(new Movie("The Cell", PriceCode.NewRelease), 3));
-            Assert.Equal("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.Statement());
+            Assert.Equal("Rental record for Fred\n\tThe Cell\t9.0\nAmount owed is 9.0\nYou earned 2 frequent renter points.", customer.Statement());
         }
 
         [Fact]
@@ -25,14 +25,14 @@ namespace VideoStore
         {
             customer.AddRental(new Rental(new Movie("The Cell", PriceCode.NewRelease), 3));
             customer.AddRental(new Rental(new Movie("The Tigger Movie", PriceCode.NewRelease), 3));
-            Assert.Equal("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.Statement());
+            Assert.Equal("Rental record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nAmount owed is 18.0\nYou earned 4 frequent renter points.", customer.Statement());
         }
 
         [Fact]
         public void testSingleChildrensStatement()
         {
             customer.AddRental(new Rental(new Movie("The Tigger Movie", PriceCode.Childrens), 3));
-            Assert.Equal("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.Statement());
+            Assert.Equal("Rental record for Fred\n\tThe Tigger Movie\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points.", customer.Statement());
         }
 
         [Fact]
@@ -42,7 +42,7 @@ namespace VideoStore
             customer.AddRental(new Rental(new Movie("8 1/2", PriceCode.Regular), 2));
             customer.AddRental(new Rental(new Movie("Eraserhead", PriceCode.Regular), 3));
 
-            Assert.Equal("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.Statement());
+            Assert.Equal("Rental record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nAmount owed is 7.5\nYou earned 3 frequent renter points.", customer.Statement());
         }
 
     }
